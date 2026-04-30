01ce31258366826b5bf06199c3f7ce1e34d2e9df
Yogita Soni <ysoni@redhat.com>
2026-04-30 20:13:19 +0530

    fullstack_kubernetes_deploy role added

---
+++ roles/fullstack_kubernetes_deploy/files/pipeline.groovy
@@ -0,0 +1,32 @@
+import jenkins.model.*
+import org.jenkinsci.plugins.workflow.job.WorkflowJob
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
+
+def instance = Jenkins.instanceOrNull
+
+sleep(15000)
+
+def jobName = "kubernetes-minikube-app-deploy"
+def job = instance.getItem(jobName)
+
+if (job == null) {
+    job = instance.createProject(WorkflowJob, jobName)
+}
+
+// Read Jenkinsfile from local system (created by Ansible)
+def pipelineScript = new File("/var/lib/jenkins/Jenkinsfile").text
+
+// Use inline pipeline (NO SCM now)
+def definition = new CpsFlowDefinition(pipelineScript, true)
+
+job.setDefinition(definition)
+job.save()
+
+// Trigger build
+if (job != null) {
+    job.scheduleBuild2(0)
+}
+
+instance.save()
+
+println("✅ Pipeline loaded from Ansible template successfully!")
